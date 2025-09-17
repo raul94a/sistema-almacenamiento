3826dbe194a9d3e04ce02e5c6d9c3a735b9b955f
Raul Albín <raul200794@gmail.com>
2025-09-17 13:35:12 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import
+(
+	// "database/sql"
+	// "github.com/gin-gonic/gin"
+	// "go.uber.org/zap"
+	// "github.com/joho/godotenv"
+	"fmt"
+)
+
+func main()  {
+	fmt.Println("🇪🇸 🇪🇸 🇪🇸 🇪🇸  Hello World! 🇪🇸 🇪🇸 🇪🇸 🇪🇸")
+}
