d32edc47e2824c6d12d6390fa6877c6764ff85c6
Raul Albín <raul200794@gmail.com>
2025-10-11 19:00:16 +0200

    add sqlite support

---
+++ database/sqlite3.go
@@ -0,0 +1,37 @@
+// Copyright 2025 Raul Albín Alba
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package database
+
+import (
+	"database/sql"
+
+	_"github.com/mattn/go-sqlite3"
+
+)
+
+type SqliteLoader struct{}
+
+func (m SqliteLoader) LoadDatabase(connectionString string) (*sql.DB, error) {
+	// EXAMPLE file:test.db?cache=shared&mode=memory
+
+	db, err := sql.Open("sqlite3",connectionString)
+
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	// https://github.com/go-sql-driver/mysql
+	return db, err
+}
