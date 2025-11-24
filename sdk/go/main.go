2d5eeee37c10bdbd62d63a2ece2b665ec0c96b7e
Raul Albín <raul200794@gmail.com>
2025-11-25 00:28:52 +0100

    add sdk_server to test client sdks

---
+++ sdk/go/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"storage.client.com/repository"
+	"storage.client.com/repository/models"
+)
+
+
+func main(){
+
+	r := repository.NewRepository("http://localhost:4444")
+	
+	file := "bak.ipa"
+
+	m := &models.Object{
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Public: true,
+		Deleted: false,
+		Filename: "go.sum",
+		Extension: ".sum",
+		Disk: "C",
+		Location: "ya lo averiguare",
+		Hash: "No se",
+
+
+	}
+
+	bytes,err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	uid, err := r.PutObject(*m,bytes,nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("UU
+	ID %s",uid)
+
+}
--- sdk/go/repository/storage_client.go
+++ sdk/go/repository/storage_client.go
@@ -53,18 +53,18 @@ func (c *StorageRepository) addHeaders(jwt *string, req *http.Request)(*http.Req
 	if err != nil {
 		return nil,err
 	}
-	rsaEncryptedAes, err := c.cryptoutils.RsaEncryptAesKey(c.PublicKey,aesKey)
+	//rsaEncryptedAes, err := c.cryptoutils.RsaEncryptAesKey(c.PublicKey,aesKey)
 	if err != nil {
 		return nil,err
 	}
-	hexEncryptedAes := c.cryptoutils.HexEncodeEncryptedAesKey(rsaEncryptedAes)
+	//hexEncryptedAes := c.cryptoutils.HexEncodeEncryptedAesKey(rsaEncryptedAes)
 	hexEncodedAesPacket := c.cryptoutils.HexEncodeAesPacket(packet)
 	hashNonce := c.cryptoutils.HashNonce(packet.Data)
 	builder = *builder.Builder(req).
 	AddSite().
 	AddDigitalEnvelope(hexEncodedAesPacket.Data).
 	AddNonce(hexEncodedAesPacket.Nonce).
-	AddAuth(hexEncryptedAes).
+	//AddAuth(hexEncryptedAes).
 	AddHashHeader(hashNonce)
 
 	if jwt != nil {
